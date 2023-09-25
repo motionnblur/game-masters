b9153291f28a7f6977a703afaacbfe6bbf45f8e4
chill <chill@mail.com>
2023-09-25 18:37:33 +0300

    added Signup form

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
 "use client";
 import { useState } from "react";
 import Login from "../../components/Login";
+import Signup from "../../components/Signup";
 export default function Home() {
   const [loginForm, setLoginForm] = useState(false);
+  const [signupForm, setSignupForm] = useState(false);
   const openLoginForm = () => {
     setLoginForm(true);
   };
   const closeLoginForm = () => {
     setLoginForm(false);
+  };
+  const openSignupForm = () => {
+    setSignupForm(true);
+  };
+  const closeSignupForm = () => {
+    setSignupForm(false);
   };
   return (
     <div className="w-full h-full bg-slate-800 flex">
@@ -29,15 +37,31 @@ export default function Home() {
             </div>
             <div className="h-5/6 w-full flex justify-center items-center">
               <div className="bg-slate-600 h-1/3 w-2/3 mb-32 flex justify-center items-center gap-5">
-                <b className="text-2xl text-slate-200 cursor-pointer">Lol</b>
-                <b className="text-2xl text-slate-200 cursor-pointer">Dota</b>
-                <b className="text-2xl text-slate-200 cursor-pointer">Cs Go</b>
+                <b
+                  className="text-2xl text-slate-200 cursor-pointer"
+                  onClick={openSignupForm}
+                >
+                  Lol
+                </b>
+                <b
+                  className="text-2xl text-slate-200 cursor-pointer"
+                  onClick={openSignupForm}
+                >
+                  Dota
+                </b>
+                <b
+                  className="text-2xl text-slate-200 cursor-pointer"
+                  onClick={openSignupForm}
+                >
+                  Cs Go
+                </b>
               </div>
             </div>
           </div>
         </div>
       </div>
       {loginForm && <Login closeLoginForm={closeLoginForm} />}
+      {signupForm && <Signup closeSignupForm={closeSignupForm} />}
     </div>
   );
 }
