05216d6f883ec4b389061fa9f0d4d3ba2dcbffc6
chill <chill@mail.com>
2023-09-26 16:52:00 +0300

    added password hashing

--- server/main/src/main/java/com/main/mainserver/controller/UserController.java
+++ server/main/src/main/java/com/main/mainserver/controller/UserController.java
@@ -3,6 +3,7 @@ package com.main.mainserver.controller;
 import com.main.mainserver.entity.UserEntity;
 import com.main.mainserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @PostMapping("/api/create_user")
     private String createUser(@RequestBody UserEntity userEntity) {
         try {
-            return userService.saveUser(userEntity);
+            String hashedPassword = passwordEncoder.encode(userEntity.getPassw());
+
+            UserEntity newUser = new UserEntity();
+            newUser.setUserName(userEntity.getUserName());
+            newUser.setLastName(userEntity.getLastName());
+            newUser.setMail(userEntity.getMail());
+            newUser.setPassw(hashedPassword);
+
+            return userService.saveUser(newUser);
         } catch (Exception e) {
             return "error";
         }
