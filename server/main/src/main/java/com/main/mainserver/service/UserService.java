bbdefe275ab5b5755ba1d26be565cfec8224ffd0
chill <chill@mail.com>
2023-09-26 12:52:55 +0300

    added user service

---
+++ server/main/src/main/java/com/main/mainserver/service/UserService.java
@@ -0,0 +1,24 @@
+package com.main.mainserver.service;
+
+import com.main.mainserver.entity.UserEntity;
+import com.main.mainserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    public String saveUser(UserEntity userEntity){
+        return userRepository.save(userEntity).getUserName();
+    }
+    public void deleteUser(UserEntity userEntity){
+        userRepository.delete(userEntity);
+    }
+    public void updateUser(UserEntity userEntity){
+        userRepository.save(userEntity);
+    }
+    public UserEntity findUser(String userName){
+        return userRepository.findByUserName(userName);
+    }
+}
