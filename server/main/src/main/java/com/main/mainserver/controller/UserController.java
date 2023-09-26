2696abd8760947eb48c6aa4d0616e5dbcb48e5af
chill <chill@mail.com>
2023-09-26 15:14:42 +0300

    working as a mvp for now

--- server/main/src/main/java/com/main/mainserver/controller/UserController.java
+++ server/main/src/main/java/com/main/mainserver/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.main.mainserver.controller;
 
 import com.main.mainserver.entity.UserEntity;
+import com.main.mainserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,11 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserService userService;
     @PostMapping("/api/create_user")
     private String createUser(@RequestBody UserEntity userEntity) {
         try {
-            //return userService.saveUser(userEntity);
-            return "ok";
+            return userService.saveUser(userEntity);
         } catch (Exception e) {
             return "error";
         }
