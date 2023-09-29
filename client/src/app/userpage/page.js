ce09337ec026f7d8dd64b8c704dccd5dc3e712e8
chill <chill@mail.com>
2023-09-29 14:27:04 +0300

    added sole welcome text with cookies

--- client/src/app/userpage/page.js
+++ client/src/app/userpage/page.js
@@ -1,18 +1,20 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { getCookie } from "cookies-next";
 
 export default function page() {
   const url = "http://localhost:8080/api/authenticate";
+  const [userName, setUserName] = useState("");
   useEffect(() => {
-    const cookieVal = getCookie("user-id");
-    alert(cookieVal);
-  }, []);
+    setUserName(getCookie("user-id"));
+  }, [userName]);
   return (
     <div className="w-full h-full bg-slate-800 flex">
       <div className="m-2 w-full bg-slate-400 flex items-center flex-col">
-        <div className="w-full h-14 bg-slate-300"></div>
+        <div className="w-full h-14 bg-slate-300">
+          <b>Welcome {userName}</b>
+        </div>
       </div>
     </div>
   );
