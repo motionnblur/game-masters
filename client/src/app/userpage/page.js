bd20668ce49e0c7cf7c36f3bd8159bb9ce76d401
carbonfiberdev <carbonfiberdev@gmail.com>
2023-12-04 01:38:25 +0300

    some refactoring

--- client/src/app/userpage/page.js
+++ client/src/app/userpage/page.js
@@ -3,15 +3,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { getCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
-import { isVideoFile } from "../../../api/Regex";
+
+const url = "http://localhost:8080/api/authenticate";
 
 export default function page() {
   const router = useRouter();
-  const url = "http://localhost:8080/api/authenticate";
   const [userName, setUserName] = useState("");
-
-  const [file, setFile] = useState();
-  const [getTable, setGetTable] = useState(false);
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
@@ -30,7 +27,6 @@ export default function page() {
         }
       )
       .then((res) => {
-        console.log(res.data);
         if (res.data) {
           setUserName(res.data);
         } else {
@@ -38,39 +34,8 @@ export default function page() {
           router.push("/");
         }
       });
-  }, []);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("userName", userName);
-
-    if (!isVideoFile(file.name)) {
-      console.log("it's not a video file");
-      //return;
-    }
-
-    axios
-      .post("http://localhost:8081/upload", formData, {
-        withCredentials: true,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((res) => {
-        console.log(res.status);
-      });
-  };
-
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
-  };
-
-  const getUploadTable = () => {
     if (videos.length > 0) setVideos([]);
-    setGetTable(!getTable);
     axios
       .get("http://localhost:8080/api/getUploadTable", {
         params: {
@@ -88,28 +53,18 @@ export default function page() {
           ]);
         });
       });
-  };
+  }, [userName]);
 
   return (
-    <div className="w-full h-full bg-slate-800 flex">
-      <div className="m-2 w-full bg-slate-400 flex items-center flex-col">
-        <div className="w-full h-14 bg-slate-300 flex items-center justify-center">
-          <b>{userName}</b>
-        </div>
+    <div className="flex flex-col justify-center align-middle items-center">
+      <b>{userName}</b>
+      <br />
+      <b>## your videos ##</b>
+      {videos.map((item) => (
         <>
-          <form onSubmit={handleSubmit}>
-            <input type="file" name="file" onChange={handleFileChange} />
-            <input type="submit" value="Upload" />
-          </form>
+          File name: {item.fileName}, path: {item.filePath} <br />
         </>
-        <button onClick={getUploadTable}>Click to see your videos</button>
-        {getTable &&
-          videos.map((item) => (
-            <>
-              File name: {item.fileName}, path: {item.filePath} <br />
-            </>
-          ))}
-      </div>
+      ))}
     </div>
   );
 }
