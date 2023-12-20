c2931cdf6239a21c3c60bf4cd18d262cb15d7630
chill <chill@mail.com>
2023-12-20 16:17:13 +0300

    some refactoring

--- client/src/app/uploadpage/page.js
+++ client/src/app/uploadpage/page.js
@@ -71,12 +71,12 @@ export default function page() {
 
   const UploadStatus = () => {
     if (fileSelected === false) {
-      return <b>Select a file to upload</b>;
+      return <b className="mt-3">Select a file to upload</b>;
     }
     if (uploadPercentage !== 100) {
       return <progress value={uploadPercentage / 100} />;
     } else {
-      return <b>Upload Completed</b>;
+      return <b className="mt-3">Upload Completed</b>;
     }
   };
 
@@ -96,14 +96,13 @@ export default function page() {
         </div>
         <>
           <form onSubmit={handleSubmit}>
-            <div className="w-full h-full flex flex-col items-center justify-center mt-2">
+            <div className="w-full h-full flex flex-col items-center justify-center mt-2 gap-2">
               <input
                 class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                 type="file"
                 name="file"
                 onChange={handleFileChange}
               />
-              <br />
               <button
                 class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-3 border border-gray-400 rounded shadow"
                 type="submit"
@@ -114,7 +113,7 @@ export default function page() {
             </div>
           </form>
         </>
-        <br />
+
         <UploadStatus />
       </div>
     </div>
