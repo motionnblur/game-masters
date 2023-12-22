aca4af27b81f935b3d8732f07a5e79497d172820
carbonfiberdev <carbonfiberdev@gmail.com>
2023-12-22 23:09:22 +0300

    refactoring

--- client/components/VideoImage.js
+++ client/components/VideoImage.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function VideoImage(props) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const showImageMenu = () => {
+    setShowMenu(true);
+  };
+
+  const hideImageMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
-    <>
+    <div style={{ position: "relative", display: "inline-block" }}>
       <img
         key={props.index}
         src={props.data}
-        style={{ width: "100%", marginBottom: "0.5rem" }}
+        style={{ width: "100%" }}
+        onMouseEnter={showImageMenu}
       />
-    </>
+      {showMenu && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            zIndex: 10,
+          }}
+          onMouseLeave={hideImageMenu}
+        />
+      )}
+    </div>
   );
 }
