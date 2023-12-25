c2a2369b9675426d01cd505ec1beea2658a551b1
chill <chill@mail.com>
2023-12-26 00:08:30 +0300

    .

--- client/components/MenuItem.js
+++ client/components/MenuItem.js
@@ -6,9 +6,15 @@ import { setmenustate } from "../menuStateSlice";
 export default function MenuItem(props) {
   const currentstate = useSelector((state) => state.menustate.value);
   const dispatch = useDispatch();
+  var color = "bg-slate-100";
+
+  if (currentstate === 0 && props.id === 0) {
+    //alert("ok");
+  }
+
   return (
     <div
-      className="w-8 h-8 bg-slate-100 rounded-full cursor-pointer hover:scale-90 transition duration-2000 ease-in-out"
+      className={`w-8 h-8 ${color} rounded-full cursor-pointer hover:scale-90 transition duration-2000 ease-in-out`}
       onClick={() => {
         dispatch(setmenustate(props.id));
       }}
