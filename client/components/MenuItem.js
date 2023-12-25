b1556c05e1c51cd36a9c1effe001e1efd7239c41
chill <chill@mail.com>
2023-12-25 15:08:26 +0300

    .

---
+++ client/components/MenuItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function MenuItem() {
+  return (
+    <div className=" w-8 h-8 bg-slate-100 rounded-full cursor-pointer"></div>
+  );
+}
--- client/components/Menubar.js
+++ client/components/Menubar.js
@@ -1,14 +1,15 @@
 import React from "react";
+import MenuItem from "./MenuItem";
 
 export default function Menubar() {
   return (
     <div className="hidden absolute w-16 h-full md:flex justify-center items-center">
       <div className="w-10 h-96 bg-slate-700 rounded-2xl flex flex-col justify-center items-center gap-10">
-        <div className=" w-8 h-8 bg-slate-100 rounded-full cursor-pointer"></div>
-        <div className=" w-8 h-8 bg-slate-100 rounded-full cursor-pointer"></div>
-        <div className=" w-8 h-8 bg-slate-100 rounded-full cursor-pointer"></div>
-        <div className=" w-8 h-8 bg-slate-100 rounded-full cursor-pointer"></div>
-        <div className=" w-8 h-8 bg-slate-100 rounded-full cursor-pointer"></div>
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
       </div>
     </div>
   );
