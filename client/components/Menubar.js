796d87e112c03cc3e609385af44ad0bf87789c93
chill <chill@mail.com>
2023-12-25 14:59:39 +0300

    .

--- client/components/Menubar.js
+++ client/components/Menubar.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Menubar() {
-  return <div className="absolute w-14 h-full flex"></div>;
+  return (
+    <div className="hidden absolute w-14 h-full md:flex justify-center items-center">
+      <div className="w-10 h-96 bg-slate-700 rounded-2xl"></div>
+    </div>
+  );
 }
