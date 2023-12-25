dd854aaaf68809ef0ea2ebd6efac33d18f58ec31
chill <chill@mail.com>
2023-12-25 14:24:51 +0300

    .

---
+++ client/src/app/destination/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="w-full h-full">
+      <div
+        className="w-full h-full p-3
+        md:p-3 md:pl-52 md:pr-52"
+      ></div>
+    </div>
+  );
+}
