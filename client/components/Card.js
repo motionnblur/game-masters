0a7fe361cfa779558908abe5d3087c5ce591ef5f
chill <chill@mail.com>
2023-12-25 23:08:26 +0300

    added card

---
+++ client/components/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className=" w-48 h-64 bg-slate-700 rounded-md shadow-black shadow-sm"></div>
+  );
+}
--- client/src/app/destination/page.js
+++ client/src/app/destination/page.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../../../components/Navbar";
 import Menubar from "../../../components/Menubar";
+import Card from "../../../components/Card";
 
 export default function page() {
   console.log("page");
@@ -8,7 +8,9 @@ export default function page() {
     <>
       <Menubar />
       <div className="w-full h-full flex flex-col items-center justify-center sm:gap-3 bg-slate-200">
-        <div className="w-[100vh] h-[65vh] bg-slate-300"></div>
+        <div className="w-[100vh] h-[65vh] bg-slate-300 flex">
+          <Card />
+        </div>
       </div>
     </>
   );
