b10eb46d8872a6a981b50ba0c6c42c8d41f6caf1
chill <chill@mail.com>
2023-12-25 22:47:05 +0300

    refactoring

--- client/src/app/destination/page.js
+++ client/src/app/destination/page.js
@@ -7,13 +7,7 @@ export default function page() {
   return (
     <>
       <Menubar />
-      <div className="w-full h-full sm:p-3 sm:pl-4 sm:pr-4 md:pl-16 md:pr-16 xl:pl-32 xl:pr-32 2xl:pl-72 2xl:pr-72 flex flex-col sm:gap-3 bg-cyan-600">
-        <Navbar />
-        <div
-          id="main-body"
-          className="w-full h-full bg-slate-900 sm:rounded-md"
-        ></div>
-      </div>
+      <div className="w-full h-full flex flex-col sm:gap-3 bg-cyan-600"></div>
     </>
   );
 }
