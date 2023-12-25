5dd5af8a1981a3f416abd2d5fe471c50f64591a1
chill <chill@mail.com>
2023-12-25 22:35:51 +0300

    added shadow

--- client/components/Menubar.js
+++ client/components/Menubar.js
@@ -5,7 +5,7 @@ import { Video, Home, Settings, PieChart } from "react-feather";
 export default function Menubar() {
   return (
     <div className="hidden absolute w-16 h-full md:flex justify-center items-center">
-      <div className="w-11 h-72 bg-slate-700 rounded-3xl flex flex-col justify-center items-center gap-12">
+      <div className="w-11 h-72 bg-slate-700 rounded-3xl flex flex-col justify-center items-center gap-12 shadow-black shadow-sm">
         <MenuItem icon={<Home />} />
         <MenuItem icon={<Video />} />
         <MenuItem icon={<PieChart />} />
