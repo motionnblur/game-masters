6d212703e539b5bb7f0311809dbb2e5e4fbf7465
chill <chill@mail.com>
2023-12-25 15:16:09 +0300

    .

--- client/components/Menubar.js
+++ client/components/Menubar.js
@@ -4,7 +4,7 @@ import MenuItem from "./MenuItem";
 export default function Menubar() {
   return (
     <div className="hidden absolute w-16 h-full md:flex justify-center items-center">
-      <div className="w-10 h-96 bg-slate-700 rounded-2xl flex flex-col justify-center items-center gap-10">
+      <div className="w-10 h-96 bg-slate-700 rounded-2xl flex flex-col justify-center items-center gap-9">
         <MenuItem />
         <MenuItem />
         <MenuItem />
