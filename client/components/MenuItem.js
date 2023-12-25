e7bb990ee5a0be0a3b2d4c5c686895422d07e3ff
chill <chill@mail.com>
2023-12-25 19:20:27 +0300

    .

--- client/components/MenuItem.js
+++ client/components/MenuItem.js
@@ -5,7 +5,6 @@ import { increment } from "../counterSlice";
 
 export default function MenuItem() {
   const dispatch = useDispatch();
-  console.log("menu item");
   return (
     <div
       className=" w-8 h-8 bg-slate-100 rounded-full cursor-pointer"
