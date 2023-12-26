0dea0c536f295242300d01ff8beda8d71a8523d7
chill <chill@mail.com>
2023-12-26 14:56:49 +0300

    added input fields

--- client/src/app/destination/page.js
+++ client/src/app/destination/page.js
@@ -15,11 +15,11 @@ export default function page() {
         </div>
         {currentstate && (
           <div
-            className="absolute w-[32vh] h-[45vh] z-30 bg-cyan-500 rounded-md shadow-slate-500
+            className="absolute w-[32vh] h-[42vh] z-30 bg-cyan-500 rounded-md shadow-slate-500
            shadow-md flex flex-col p-5 gap-3"
           >
             <div className="w-full h-full flex flex-col justify-center items-center gap-4">
-              <div className="w-full h-14  rounded-md flex flex-row gap-2">
+              <div className="w-full h-11  rounded-md flex flex-row gap-2">
                 <button className="w-full h-full bg-slate-50 rounded-md">
                   Login
                 </button>
@@ -27,8 +27,22 @@ export default function page() {
                   Sign up
                 </button>
               </div>
-              <div className="w-full h-14 bg-slate-300 rounded-md"></div>
-              <div className="w-full h-14 bg-slate-300 rounded-md"></div>
+              <div className="w-full h-14 bg-slate-300 rounded-md p-2">
+                <input
+                  type="text"
+                  id="mail"
+                  name="mail"
+                  className="w-full h-full bg-slate-300 rounded-md outline-none"
+                />
+              </div>
+              <div className="w-full h-14 bg-slate-300 rounded-md p-2">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="w-full h-full bg-slate-300 rounded-md outline-none"
+                />
+              </div>
             </div>
             <div className="w-full h-24 flex justify-center items-center">
               <button className="w-full h-12 bg-slate-500 rounded-md">
