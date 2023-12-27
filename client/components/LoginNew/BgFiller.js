00d7d9c697c4c9b7732e1625c9e5abb79fe873b7
chill <chill@mail.com>
2023-12-27 11:44:28 +0300

    added bgfiller ui

---
+++ client/components/LoginNew/BgFiller.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BgFiller() {
+  return <div className="absolute w-full h-full bg-black opacity-60" />;
+}
--- client/src/app/destination/page.js
+++ client/src/app/destination/page.js
@@ -3,12 +3,14 @@ import React from "react";
 import Card from "../../../components/Card";
 import { useSelector } from "react-redux";
 import LoginNew from "../../../components/LoginNew/LoginNew";
+import BgFiller from "../../../components/LoginNew/BgFiller";
 
 export default function page() {
   const currentstate = useSelector((state) => state.loginstate.value);
 
   return (
     <>
+      {currentstate && <BgFiller />}
       <div className="w-full h-full flex flex-col items-center justify-center sm:gap-3 bg-slate-200">
         <div className="w-[70vh] h-[40vh] flex gap-4 justify-center items-center">
           <Card text={<b>lol</b>} id={0} />
