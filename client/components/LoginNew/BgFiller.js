23b677672595df7d7a68f5931403aa64bb0ecae2
chill <chill@mail.com>
2023-12-27 11:58:01 +0300

    added closing bgfiller with onclick using redux

--- client/components/LoginNew/BgFiller.js
+++ client/components/LoginNew/BgFiller.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setloginstate } from "../../states/loginStateSlice";
 
 export default function BgFiller() {
-  return <div className="absolute w-full h-full bg-black opacity-60" />;
+  const dispatch = useDispatch();
+  return (
+    <div
+      className="absolute w-full h-full bg-black opacity-60"
+      onClick={() => {
+        dispatch(setloginstate(false));
+      }}
+    />
+  );
 }
