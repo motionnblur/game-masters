5c97f76a5f4d9c7b14fc9373169cfb2352625c74
carbonfiberdev <carbonfiberdev@gmail.com>
2023-12-29 17:32:09 +0300

    the player works like a charm between all my devices !

--- client/components/Player.js
+++ client/components/Player.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Player() {
   return (
     <video width={400} controls>
-      <source src="http://localhost:8081/getFile/test.webm" type="video/webm" />
+      <source
+        src="http://192.168.1.106:8081/getFile/test.webm"
+        type="video/webm"
+      />
     </video>
   );
 }
