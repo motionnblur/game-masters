69e5009d949a9e6e5c459487b1498771b9678cf4
carbonfiberdev <carbonfiberdev@gmail.com>
2024-01-14 08:15:00 +0300

    .

--- client/src/app/userpage/page.js
+++ client/src/app/userpage/page.js
@@ -114,7 +114,10 @@ export default function Page() {
           </div>
         </div>
         {showVideo && (
-          <div className="absolute z-50 w-full h-full bg-gray-950 flex justify-center items-center bg-opacity-90">
+          <div
+            className="absolute z-50 w-full h-full bg-gray-950 flex justify-center items-center bg-opacity-90"
+            onClick={() => setShowVideo(false)}
+          >
             <div>
               <Player
                 src={`http://localhost:8081/getFile/${userName}/${videoName}`}
