ab609267e02d73b46069cd2fb7e07f535e448f7c
carbonfiberdev <carbonfiberdev@gmail.com>
2024-01-18 07:53:41 +0300

    added all videos to userpage and works

--- client/src/app/videospage/page.js
+++ client/src/app/videospage/page.js
@@ -4,16 +4,36 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Player from "../../../components/Player";
 
-export default function page() {
-  useEffect(() => {
-    const fetchVideoData = async () => {
-      const response = await axios.get(
-        "http://localhost:8080/api/getAllVideos"
-      );
-      console.log(response.data);
-    };
+function extractStrings(input) {
+  const regex = /^\/App\/upload-dir\/(.*?)\/(.*)$/;
+  const match = input.match(regex);
 
-    fetchVideoData();
-  });
-  return <Player />;
+  if (match) {
+    const A = match[1];
+    const B = match[2];
+
+    return [A, B];
+  }
+
+  return [null, null];
+}
+
+export default function page() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/getAllVideos").then((res) => {
+      setVideos(res.data);
+      console.log(res.data);
+      const [A, B] = extractStrings(res.data[0].filePath);
+    });
+  }, []);
+  return (
+    <>
+      {videos.map((video) => {
+        const [A, B] = extractStrings(video.filePath);
+
+        return <Player src={`http://localhost:8081/getFile/${A}/${B}`} />;
+      })}
+    </>
+  );
 }
