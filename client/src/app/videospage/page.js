5fd97b9b82043266e313b03f3e30b41594f061b6
carbonfiberdev <carbonfiberdev@gmail.com>
2024-01-18 06:28:48 +0300

    .

--- client/src/app/videospage/page.js
+++ client/src/app/videospage/page.js
@@ -1,6 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Player from "../../../components/Player";
 
 export default function page() {
+  useEffect(() => {
+    const fetchVideoData = async () => {
+      const response = await axios.get(
+        "http://localhost:8080/api/getAllVideos"
+      );
+      console.log(response.data);
+    };
+
+    fetchVideoData();
+  });
   return <Player />;
 }
---
+++ server/main/src/main/java/com/main/mainserver/dao/VideoDao.java
@@ -0,0 +1,12 @@
+package com.main.mainserver.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VideoDao {
+    String userName;
+    String videoName;
+    String videoPath;
+}
