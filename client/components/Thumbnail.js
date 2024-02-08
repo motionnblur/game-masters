47e61f83f4f41b93b708c7e99d560369c4002615
chill <chill@mail.com>
2024-02-08 08:59:02 +0300

    added Idle and Active properties to Thumbnail.js component

--- client/components/Thumbnail.js
+++ client/components/Thumbnail.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Thumbnail(props) {
-  return <img src={props.data} width="300" height="300" />;
+  const [click, setClick] = useState(false);
+  const OnClick = () => {
+    setClick((current) => !current);
+  };
+  const Idle = () => {
+    return <img src={props.data} width="300" height="300" onClick={OnClick} />;
+  };
+  const Active = () => {
+    return <img src={props.data} width="500" height="500" onClick={OnClick} />;
+  };
+
+  {
+  }
+  return click ? <Active /> : <Idle />;
 }
