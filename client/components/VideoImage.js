fdf0bd448361d443eeb9dec97191f917596796d2
chill <chill@mail.com>
2024-02-08 20:25:59 +0300

    deleted unused change thumbnail button from VideoImage.js component

--- client/components/VideoImage.js
+++ client/components/VideoImage.js
@@ -47,17 +47,6 @@ export default function VideoImage(props) {
             >
               Play
             </button>
-            <button
-              type="button"
-              className="text-gray-900 bg-white border 
-            border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 
-            focus:ring-gray-200 font-medium rounded-full text-sm px-4 py-2
-            dark:bg-gray-800 dark:text-white dark:border-gray-600 
-            dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700
-            cursor-pointer mt-20"
-            >
-              Change Thumbnail
-            </button>
           </div>
         </div>
       </>
