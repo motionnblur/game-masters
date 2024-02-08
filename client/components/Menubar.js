72f5ce3b13978c7f7305a6d0f5a9bd24818aa4b3
carbonfiberdev <carbonfiberdev@gmail.com>
2024-02-08 17:53:33 +0300

    added z-50 to menubar

--- client/components/Menubar.js
+++ client/components/Menubar.js
@@ -4,7 +4,7 @@ import { Video, Home, Settings, Upload } from "react-feather";
 
 export default function Menubar() {
   return (
-    <div className="hidden absolute w-16 h-full md:flex justify-center items-center">
+    <div className="hidden absolute w-16 h-full md:flex justify-center items-center z-50">
       <div className="w-11 h-72 bg-slate-700 rounded-3xl flex flex-col justify-center items-center gap-12 shadow-black shadow-sm">
         <MenuItem icon={<Home />} id={0} />
         <MenuItem icon={<Video />} id={1} />
