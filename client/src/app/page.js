d2fbc78357512a7cbe2a4c3040f8f486864eb497
chill <chill@mail.com>
2024-02-10 17:57:43 +0300

    added warriors.mp4 to main page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -13,8 +13,22 @@ export default function page() {
     return (
       <>
         {currentstate && <BgFiller />}
-        <div className="w-full h-full flex flex-col items-center justify-center sm:gap-3 bg-gray-800">
-          <div className="w-[70vh] h-[40vh] flex gap-4 justify-center items-center">
+        <div className="w-full h-full flex flex-col items-center justify-center sm:gap-3 bg-black z-10 overflow-hidden">
+          <video
+            controls={false}
+            autoPlay
+            loop={true}
+            src="warriors.mp4"
+            style={{
+              zIndex: "0",
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: "0px",
+              objectFit: "cover",
+            }}
+          />
+          <div className="w-[70vh] h-[40vh] flex gap-4 justify-center items-center z-10">
             <Card id={0} img={"lol.jpeg"} height={28} width={28} />
             <Card id={1} img={"dota.jpg"} />
           </div>
