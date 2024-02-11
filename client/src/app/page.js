a38794ee3719949b27c42305c27fb33e39d39072
chill <chill@mail.com>
2024-02-11 14:25:03 +0300

    refactoring on states for mainpage

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,33 +6,47 @@ import LoginNew from "../../components/LoginNew/Parent/LoginNew";
 import BgFiller from "../../components/LoginNew/Childrens/BgFiller";
 
 export default function page() {
-  const currentstate = useSelector((state) => state.loginstate.value);
   const userLoginState = useSelector((state) => state.userloginstate.value);
 
-  const BeforeLogin = () => {
+  const VideoLayer = () => {
+    return (
+      <>
+        <video
+          controls={false}
+          autoPlay
+          loop={true}
+          src="warriors.mp4"
+          style={{
+            zIndex: "0",
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            top: "0px",
+            objectFit: "cover",
+          }}
+        />
+      </>
+    );
+  };
+  const CardLayer = () => {
+    const currentstate = useSelector((state) => state.loginstate.value);
     return (
       <>
         {currentstate && <BgFiller />}
+        <div className="w-[70vh] h-[40vh] flex gap-4 justify-center items-center z-10">
+          <Card id={0} img={"lol.jpeg"} height={28} width={28} />
+          <Card id={1} img={"dota.jpg"} />
+        </div>
+        {currentstate && <LoginNew />}
+      </>
+    );
+  };
+  const BeforeLogin = () => {
+    return (
+      <>
         <div className="w-full h-full flex flex-col items-center justify-center sm:gap-3 bg-black z-10 overflow-hidden">
-          <video
-            controls={false}
-            autoPlay
-            loop={true}
-            src="warriors.mp4"
-            style={{
-              zIndex: "0",
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              top: "0px",
-              objectFit: "cover",
-            }}
-          />
-          <div className="w-[70vh] h-[40vh] flex gap-4 justify-center items-center z-10">
-            <Card id={0} img={"lol.jpeg"} height={28} width={28} />
-            <Card id={1} img={"dota.jpg"} />
-          </div>
-          {currentstate && <LoginNew />}
+          <VideoLayer />
+          <CardLayer />
         </div>
       </>
     );
